5428974ab239fbd074956f5170beb0a230f0532e
alexvpickering <alexvpickering@gmail.com>
2017-12-31 13:31:27 -0800

    bar chart I; migrate tern -> atom-ide + typescript

--- src/components/Play.js
+++ src/components/Play.js
@@ -2,59 +2,29 @@ import React from 'react'
 import * as d3 from "d3"
 
 
-
 class Play extends React.Component {
 
   componentDidMount() {
-    const node = this.node
-    const margin = 50,
-          height = 500 - (2 * margin),
-          width  = 500 - (2 * margin),
-          r = 10
 
-    const data = [
-            [width / 2 - r, height / 2 - r],
-            [width / 2 - r, height / 2 + r],
-            [width / 2 + r, height / 2 - r],
-            [width / 2 + r, height / 2 + r]
-          ]
+    var data = [4, 8, 15, 16, 23, 42];
 
-    const zoom = d3.zoom()
-        .scaleExtent([1, 10])
-        .on("zoom", zoomed);
+    var x = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, 420])
 
-
-    const g = d3.select(node).append("g")
-        .attr("transform", `translate(${margin}, ${margin})`)
-        .style("border", "1px solid")
-        .call(zoom)
-      .append("g")
-
-    g.selectAll("circle")
-      .data(data)
-      .enter()
-      .append("circle")
-          .attr("r", r)
-          .attr("transform", d => `translate(${d})`)
-
-function zoomed() {
-  g.attr("transform", "translate(" + d3.event.transform.x + "," + d3.event.transform.y + ") scale(" + d3.event.transform.k + ")");
-}
+    d3.select(".chart")
+      .selectAll("div")
+        .data(data)
+      .enter().append("div")
+        .style("width", d => x(d) + "px")
+        .text(d => d)
 
   }
 
 
   render() {
     return (
-    <div>
-      <svg
-        ref={node => this.node=node}
-        width="500"
-        height="500"
-        style={{border: '1px solid'}}>
-        <text></text>
-      </svg>
-    </div>
+    <div class="chart"></div>
     )
   }
 }
