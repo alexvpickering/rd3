800d13dca6b9e5baba6ead7f8d74f4b530e21432
alexvpickering <alexvpickering@gmail.com>
2017-12-31 17:24:35 -0800

    bar chart II

--- src/components/Play.js
+++ src/components/Play.js
@@ -8,23 +8,42 @@ class Play extends React.Component {
 
     var data = [4, 8, 15, 16, 23, 42];
 
-    var x = d3.scaleLinear()
-        .domain([0, d3.max(data)])
-        .range([0, 420])
+    var width = 420,
+        barHeight = 20
 
-    d3.select(".chart")
-      .selectAll("div")
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, width])
+
+    var chart = d3.select(".chart")
+      .append("svg")
+        .attr("width", width)
+        .attr("height", barHeight * data.length)
+
+    var bar = chart.selectAll("g")
         .data(data)
-      .enter().append("div")
-        .style("width", d => x(d) + "px")
+      .enter().append("g")
+        .attr("transform", (d, i) => `translate(0, ${barHeight * i})`)
+
+    bar.append("rect")
+        .attr("width", d => x(d))
+        .attr("height", barHeight - 1)
+
+
+    bar.append("text")
+        .attr("x", d => x(d) - 3)
+        .attr("y", barHeight/2)
+        .attr("dy", ".35em")
         .text(d => d)
+
+    console.log(bar)
 
   }
 
 
   render() {
     return (
-    <div class="chart"></div>
+    <div className="chart"></div>
     )
   }
 }
